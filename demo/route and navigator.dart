e2eadad2a98caa0f66364826d5430c894cb0dac4
孙广彪 <2991552132@qq.com>
2018-09-17 23:22:04 +0800

    update route and navigator

--- demo/route and navigator.dart
+++ demo/route and navigator.dart
@@ -11,13 +11,12 @@ void main() {
   if (Platform.isAndroid) {
     // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
     SystemUiOverlayStyle systemUiOverlayStyle =
-    SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   }
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -26,6 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: home(),
+      // 路由命名
       routes: <String, WidgetBuilder>{
         '/router/second': (_) => second(),
         '/router/third': (_) => third(),
@@ -34,41 +34,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 第3页，navigator pop时传递“message from third pages”
+// 接受参数message
 class third extends StatelessWidget {
+  third({
+    Key key,
+    this.message: '',
+  });
+  String message;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop('message from third pages');
-                },
-                child: Text('third page 返回上层'))));
+            child:  OutlineButton(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor),
+              child: Text('${message} third page 返回上层'),
+              onPressed: () {
+                Navigator.of(context).pop('message from third pages');
+              },
+            )
+        ),
+    );
   }
 }
-
+// 第2页，navigator pop时传递“message from third pages”
+// 接受参数message
 class second extends StatelessWidget {
   second({
     Key key,
-    this.title: '',
-  }) : super(key: key);
-  String title;
+    this.message: '',
+  });
+  String message;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pop('message from second pages');
-                },
-                child: Text('${title}second page 返回上层'))));
+            child: OutlineButton(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor),
+              child: Text('${message}second page 返回上层'),
+              onPressed: () {
+                Navigator.of(context).pop('message from second pages');
+              },
+            )
+        )
+    );
   }
 }
 
+// 主页
 class home extends StatelessWidget {
   static final GlobalKey<ScaffoldState> _scaffoldKey =
-  new GlobalKey<ScaffoldState>();
+      new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
@@ -76,19 +94,16 @@ class home extends StatelessWidget {
       key: _scaffoldKey,
       body: Center(
           child: OutlineButton(
-            borderSide: BorderSide(color: Theme
-                .of(context)
-                .primaryColor),
-            child: Text(
-              'open drawer',
-              style: new TextStyle(color: Theme
-                  .of(context)
-                  .primaryColor),
-            ),
-            onPressed: () {
-              _scaffoldKey.currentState.openDrawer();
-            },
-          )),
+        borderSide: BorderSide(color: Theme.of(context).primaryColor),
+        child: Text(
+          'open drawer',
+          style: new TextStyle(color: Theme.of(context).primaryColor),
+        ),
+        onPressed: () {
+          // 打开侧边栏
+          _scaffoldKey.currentState.openDrawer();
+        },
+      )),
       drawer: new Drawer(
         child: new Column(
           children: <Widget>[
@@ -97,12 +112,13 @@ class home extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.verified_user),
+              // iOS式动画至第2页
               onPressed: () {
                 Navigator
                     .of(context, rootNavigator: true)
                     .push(CupertinoPageRoute<void>(
-                  builder: (BuildContext context) => second(),
-                ));
+                      builder: (BuildContext context) => second(),
+                    ));
               },
             ),
             Container(
@@ -110,8 +126,9 @@ class home extends StatelessWidget {
             ),
             OutlineButton(
               borderSide: BorderSide(color: Colors.black),
-              child: Text('命名路由，andriod式动画',
+              child: Text('命名路由至第3页，andriod式动画',
                   style: new TextStyle(color: Colors.black)),
+              // 采用命名路由至第3页。
               onPressed: () {
                 Navigator.of(context).pushNamed('/router/third').then((value) {
                   print(value);
@@ -123,13 +140,13 @@ class home extends StatelessWidget {
             ),
             OutlineButton(
               borderSide: BorderSide(color: Colors.black),
-              child: Text('非命名路由，iOS式动画',
+              child: Text('非命名路由至第2页，iOS式动画',
                   style: new TextStyle(color: Colors.black)),
               onPressed: () {
                 Navigator.of(context).push(CupertinoPageRoute<void>(
-                  builder: (BuildContext context) =>
-                      second(title: 'title from drawer '),
-                ));
+                      builder: (BuildContext context) =>
+                          second(message: 'title from drawer '),
+                    ));
               },
             ),
             Container(
@@ -137,13 +154,13 @@ class home extends StatelessWidget {
             ),
             OutlineButton(
               borderSide: BorderSide(color: Colors.black),
-              child: Text('非命名路由，自适应式动画',
+              child: Text('非命名路由至第2页，自适应式动画',
                   style: new TextStyle(color: Colors.black)),
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute<void>(
-                  builder: (BuildContext context) =>
-                      second(title: 'title from drawer '),
-                ));
+                      builder: (BuildContext context) =>
+                          second(message: 'title from drawer '),
+                    ));
               },
             ),
             Container(
@@ -151,8 +168,8 @@ class home extends StatelessWidget {
             ),
             OutlineButton(
               borderSide: BorderSide(color: Colors.black),
-              child:
-              Text('弹出式路由 showDialog', style: new TextStyle(color: Colors.black)),
+              child: Text('弹出式路由 showDialog',
+                  style: new TextStyle(color: Colors.black)),
               onPressed: () {
                 showDialog<void>(
                   context: context,
